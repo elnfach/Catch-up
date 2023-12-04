4c96e1a650c19dbb0a590f48b1cae80de91d98ca
elnfach <vpetuhov160@gmail.com>
2023-12-04 21:35:19 +0800

    Update vector2f.cpp

--- Engine/src/Catch-up/include/components/vector2f.cpp
+++ Engine/src/Catch-up/include/components/vector2f.cpp
@@ -8,7 +8,7 @@ float Engine::Vector2f::magnitude()
 
 Engine::Vector2f Engine::Vector2f::normalized()
 {
-	return Vector2f(pow(x / magnitude(), 2) + pow(y / magnitude(), 2), 0);
+	return Vector2f(x / magnitude(), y / magnitude());
 }
 
 Engine::Vector2f::Vector2f() : x(0.0f), y(0.0f)
