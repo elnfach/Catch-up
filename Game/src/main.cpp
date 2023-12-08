7322626a5bf5fa2db370a75fecd5175035edd874
elnfach <vpetuhov160@gmail.com>
2023-12-08 23:09:06 +0800

    Update main.cpp

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -23,6 +23,8 @@ public:
 
 CatchUp::CatchUp()
 {
+	std::cout << "Catch up!\n";
+	std::cout << "Version: 0.1a\n";
 }
 
 CatchUp::~CatchUp()
@@ -35,10 +37,10 @@ void CatchUp::start()
 	wolf = new Wolf(100, 100, 25, 25);
 	hare = new Hare(100, 500, 25, 25);
 
-	top_wall = new Wall(0, 0, 1280, 10);
-	bottom_wall = new Wall(0, 710, 1280, 10);
-	left_wall = new Wall(0, 0, 10, 710);
-	right_wall = new Wall(1270, 0, 10, 710);
+	top_wall = new Wall(640, 0, 1280, 10);
+	bottom_wall = new Wall(640, 720, 1280, 10);
+	left_wall = new Wall(0, 360, 10, 710);
+	right_wall = new Wall(1270, 360, 10, 710);
 }
 void CatchUp::update(float deltaTime)
 {
